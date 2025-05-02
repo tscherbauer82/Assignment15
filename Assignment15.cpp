d427dd8e41e13c16e7978ab86a112b18526dd535
TScherbauer82 <thomas.scherbauer@sinclair.edu>
2025-05-01 22:37:26 -0400

    Build function prototype and character function to build the offset

--- Assignment15.cpp
+++ Assignment15.cpp
@@ -5,9 +5,48 @@
 //Program to calculate character offsets using exception handling
 
 #include <iostream>
+#include <cctype>
 using namespace std;
+
+//Function prototype
+char character(char, int);
+
+//Classes for the two exceptions
+class invalidCharacterException {
+public:
+	const char* message;
+	invalidCharacterException() : message("Invalid character") {}
+	
+};
+
+class invalidRangeException {
+public:
+	const char* message;
+	invalidRangeException() : message("Invalid Range") {}
+};
+
+//function for "character"
+char character(char start, int offset) {
+	if (!isalpha(start)) {
+		throw invalidCharacterException();
+	}
+
+	char result = start + offset;
+
+	if ((islower(start) && (result < 'a' || result > 'z')) || (isupper(start) && (result < 'A' || result > 'Z'))) {
+		throw invalidRangeException();
+	}
+
+	return result;
+}
 
 int main()
 {
+	try {
+		cout << "character ('a', 1): " << character("a", 1) << endl;
+	}
+	catch (exception& e{
+		cout<<"character throw exception" <<
+		})
    return 0;
 }
